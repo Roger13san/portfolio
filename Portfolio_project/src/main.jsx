6bfddfec9468cce52d92dbd4a2785a0573e10764
Roger <rogersantana269@gmail.com>
2026-02-07 16:22:29 -0300

    Subindo alterações na introdução para torna-la mais morderna

--- Portfolio_project/src/main.jsx
+++ Portfolio_project/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Introducao from './secoes/introducao/introducao'
-import Sobre_mim from './secoes/sobre_mim/sobre_mim'
+import Sobre_mim from './secoes/Sobre_mim/SobreMim'
+import Scroll from './secoes/Scroll/Scroll'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Introducao/>
     <Sobre_mim/>
+    
+    <Scroll/>
   </StrictMode>,
 )
